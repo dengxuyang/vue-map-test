3ff2e4be0caf5fd137824f95e376f864dea1f7c8
673303066@qq.com <673303066@qq.com>
2025-06-16 19:57:55 +0800

    chore: remove sensitive keys from VueMapPlugin configuration

--- src/main.js
+++ src/main.js
@@ -16,8 +16,8 @@ const app = createApp(App)
 
 app.use(VueMapPlugin, {
   amap: {
-    key: '9e3bf3f7302a2e02c7b4f8999e75ee98',
-    securityJsCode: '68e3b1cb01acd7cc950b8f4d20a0a8e6', // 如需安全密钥
+    key: '',
+    securityJsCode: '', // 如需安全密钥
     plugins: ['AMap.Geocoder'] // 需要加载的插件
   },
   bmap: {
