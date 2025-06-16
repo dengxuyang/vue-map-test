581f7b6eed6e4718d5fa36e6cc615e0878d74732
673303066@qq.com <673303066@qq.com>
2025-06-16 20:02:20 +0800

    feat: add environment variables for map integration and update main.js to use them

--- src/main.js
+++ src/main.js
@@ -2,30 +2,30 @@
  * @Author: dengxuyang
  * @Date: 2025-06-16 19:12:46
  * @LastEditors: 673303066@qq.com
- * @LastEditTime: 2025-06-16 19:17:50
+ * @LastEditTime: 2025-06-16 20:01:41
  * @FilePath: /vue-map-test/src/main.js
- * @Description: 
+ * @Description:
  */
-import './assets/main.css'
+import "./assets/main.css";
 
-import { createApp } from 'vue'
-import App from './App.vue'
-import VueMapPlugin from 'vue-map-plugin'
+import { createApp } from "vue";
+import App from "./App.vue";
+import VueMapPlugin from "vue-map-plugin";
 
-const app = createApp(App)
+const app = createApp(App);
 
 app.use(VueMapPlugin, {
   amap: {
-    key: '',
-    securityJsCode: '', // 如需安全密钥
-    plugins: ['AMap.Geocoder'] // 需要加载的插件
+    key: import.meta.env.VITE_AMAP_KEY,
+    securityJsCode: import.meta.env.VITE_AMAP_SECURITY, // 如需安全密钥
+    plugins: ["AMap.Geocoder"], // 需要加载的插件
   },
   bmap: {
-    key: '你的百度Key'
+    key: "你的百度Key",
   },
   google: {
-    key: '你的Google Key'
-  }
-})
+    key: "你的Google Key",
+  },
+});
 
-app.mount('#app')
+app.mount("#app");
